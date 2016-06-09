c0dcf3d333cd7b72508a9c09c5aa08238bf831ed
Rytis Ilciukas <zibalas@gmail.com>
2016-06-09 15:17:26 +0200

    create backup copy of template file so that container can be restarted

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 if [ "$1" = 'ucc-bin' ]; then
     # Put cdkey in place
     echo $CDKEY > cdkey
+    cp UT2004.ini.tpl copy.bak
     envtpl UT2004.ini.tpl
+    mv copy.bak UT2004.ini.tpl
 fi
 
 exec "$@"
